75fad42d5af6d1f8d38fc3bf4762ff0cece9c79a
Dawid <kontakt.dawid.zomerfeld@gmail.com>
2019-07-24 17:25:40 +0200

    poprawka + stworzenie modulu layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WelcomeComponent } from './home/welcome/welcome.component';
 import {HomeModule} from './home/home.module';
+import {MaterialModule} from './shared-module/material/material.module';
+import {AuthModule} from './auth/auth.module';
+import {SharedModule} from './shared-module/shared.module';
+import { LayoutComponent } from './layout/layout.component';
+import {LayoutModule} from './layout/layout.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
-    HomeModule
+    HomeModule,
+    MaterialModule,
+    AuthModule,
+    SharedModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {AuthModule} from './auth/auth.module';
-import {WelcomeComponent} from './welcome/welcome.component';
 import {HomeComponent} from './home.component';
 
 @NgModule({
-  declarations: [WelcomeComponent, HomeComponent],
-  exports: [WelcomeComponent, HomeComponent],
+  declarations: [HomeComponent],
+  exports: [HomeComponent],
   imports: [
     CommonModule,
-    AuthModule
   ]
 })
 export class HomeModule { }
